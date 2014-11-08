c9c4f3a295d81675974ad918587e70bcc546dcf4
yqset <setyingqing@gmail.com>
2014-11-08 15:51:08 -0800

    Added HandShakeServer

---
+++ handShakeServer.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"strconv"
+)
+
+var UsersNameId map[string]int // Mapping from UserName to Id
+var UsersIdSession map[int]int // Mapping from Id to Session
+var Sessions map[int]bool      // Current running Sessions
+
+var CurrentAvaiId int // to assign new id for new users
+
+func main() {
+	UsersIdSession = make(map[int]int)
+	UsersNameId = make(map[string]int)
+	Sessions = make(map[int]bool)
+
+	http.HandleFunc("/SessionServer", GetUserSession)
+	http.HandleFunc("/GameServer", connectToSession)
+	http.ListenAndServe(":33333", nil)
+
+}
+
+// GetUserSession expect parameter "user" and "session"
+func GetUserSession(w http.ResponseWriter, r *http.Request) {
+	usrName := r.FormValue("user")
+	usrSession, _ := strconv.Atoi(r.FormValue("session"))
+
+	if !UserExist(usrName) {
+		// User does not exist, Assign new id
+		CurrentAvaiId++
+		UsersNameId[usrName] = CurrentAvaiId
+	}
+	usrID := UsersNameId[usrName]
+
+	if usrSession == 0 {
+		usrSession = (usrID << 3 * 31) >> 2
+	} else if !Sessions[usrSession] {
+		fmt.Fprintln(w, "Passed in session does not exist")
+		return
+	}
+
+	UsersIdSession[usrID] = usrSession
+	Sessions[usrSession] = true
+
+	SessionKeys = []int{}
+	for k := range Sessions {
+		SessionKeys = append(SessionKeys, k)
+	}
+	//fmt.Fprintf(w, "Connected successfully--\n user_name: %s\n user_id: %d\n user_session: %d", usrName, UsersNameId[usrName], UsersIdSession[UsersNameId[usrName]])
+	fmt.Fprint(w, "%v", SessionKeys)
+}
+
+func UserExist(usrName string) bool {
+	if UsersNameId[usrName] == 0 {
+		return false
+	}
+	return true
+}
+
+func connectToSession(w http.ResponseWriter, r *http.Request) {
+	usrID, _ := strconv.Atoi(r.FormValue("id"))
+	usrSession, _ := strconv.Atoi(r.FormValue("session"))
+	//
+
+}
