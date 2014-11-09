50b356e750fd75f15f3ba6699b4185fbd534c1df
yqset <setyingqing@gmail.com>
2014-11-08 17:37:21 -0800

    Added Json format, /GameServer branch

--- handShakeServer.go
+++ handShakeServer.go
@@ -1,21 +1,28 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
-	//	"strconv"
+	"strconv"
 )
 
 var UsersNameId map[string]int // Mapping from UserName to Id
 var UsersIdSession map[int]int // Mapping from Id to Session
-var Sessions map[int]bool      // Current running Sessions
+var Sessions map[int][]Node    // Current running Sessions
 
 var CurrentAvaiId int // to assign new id for new users
+
+type Node struct {
+	Data      string
+	Id        int
+	Neighbors []int
+}
 
 func main() {
 	UsersIdSession = make(map[int]int)
 	UsersNameId = make(map[string]int)
-	Sessions = make(map[int]bool)
+	Sessions = make(map[int][]Node)
 
 	http.HandleFunc("/SessionServer", GetUserSession)
 	http.HandleFunc("/GameServer", connectToSession)
@@ -36,15 +43,33 @@ func GetUserSession(w http.ResponseWriter, r *http.Request) {
 
 	usrSession := (usrID << 3 * 31) >> 2
 
-	UsersIdSession[usrID] = usrSession
-	Sessions[usrSession] = true
+	// HERE IS WHERE WE PUT INITIAL GRAPH STATE INTO SESSION
+	TempData := Node{
+		Data:      "cow",
+		Id:        1,
+		Neighbors: []int{},
+	}
 
-	SessionKeys := []int{10, 20, 50}
+	UsersIdSession[usrID] = usrSession
+	Sessions[usrSession] = append(Sessions[usrSession], TempData)
+
+	SessionKeys := []int{}
 	for k := range Sessions {
 		SessionKeys = append(SessionKeys, k)
 	}
+
+	var x = struct {
+		Sessions []int
+	}{SessionKeys}
+
 	//fmt.Fprintf(w, "Connected successfully--\n user_name: %s\n user_id: %d\n user_session: %d", usrName, UsersNameId[usrName], UsersIdSession[UsersNameId[usrName]])
-	fmt.Fprintf(w, "%v", SessionKeys)
+
+	jsonFormatted, err := json.Marshal(x)
+	if err != nil {
+		fmt.Println("error:", err)
+	}
+	fmt.Printf("%s\n", jsonFormatted)
+	fmt.Fprintf(w, "%s\n", jsonFormatted)
 }
 
 func UserExist(usrName string) bool {
@@ -54,7 +79,16 @@ func UserExist(usrName string) bool {
 	return true
 }
 
+//var UsersNameId map[string]int // Mapping from UserName to Id
+//var UsersIdSession map[int]int // Mapping from Id to Session
+//var Sessions map[int][]Node    // Current running Sessions
 func connectToSession(w http.ResponseWriter, r *http.Request) {
-	//usrID, _ := strconv.Atoi(r.FormValue("id"))
-	//usrSession, _ := strconv.Atoi(r.FormValue("session"))
+	usrName := r.FormValue("name")
+	usrID := UsersNameId[usrName]
+	usrSession, _ := strconv.Atoi(r.FormValue("session"))
+	UsersIdSession[usrID] = usrSession
+
+	jsonFormattedData, _ := json.Marshal(Sessions[usrSession])
+	fmt.Printf("%s\n", jsonFormattedData)
+	fmt.Fprintf(w, "%s", jsonFormattedData)
 }
