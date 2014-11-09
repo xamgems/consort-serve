8304f70e0cd9833a5bd40923be141a18cf4c10b6
yqset <setyingqing@gmail.com>
2014-11-08 19:15:50 -0800

    Done with login and session

--- handShakeServer.go
+++ handShakeServer.go
@@ -11,7 +11,8 @@ var UsersNameId map[string]int // Mapping from UserName to Id
 var UsersIdSession map[int]int // Mapping from Id to Session
 var Sessions map[int][]Node    // Current running Sessions
 
-var CurrentAvaiId int // to assign new id for new users
+var CurrentNumOfId int      // to assign new id for new users
+var CurrentNumOfSession int // to assign new session
 
 type Node struct {
 	Data      string
@@ -24,34 +25,27 @@ func main() {
 	UsersNameId = make(map[string]int)
 	Sessions = make(map[int][]Node)
 
-	http.HandleFunc("/SessionServer", GetUserSession)
-	http.HandleFunc("/GameServer", connectToSession)
+	CurrentNumOfSession = 2
+	InitialData := ParseData()
+	for i := 1; i <= CurrentNumOfSession; i++ {
+		Sessions[i] = InitialData
+	}
+
+	http.HandleFunc("/SessionServer", LoginAndGetSession)
+	http.HandleFunc("/GameServer", ConnectToSession)
 	http.ListenAndServe(":33333", nil)
 
 }
 
 // GetUserSession expect parameter "user" and "session"
-func GetUserSession(w http.ResponseWriter, r *http.Request) {
+func LoginAndGetSession(w http.ResponseWriter, r *http.Request) {
 	usrName := r.FormValue("user")
 
 	if !UserExist(usrName) {
 		// User does not exist, Assign new id
-		CurrentAvaiId++
-		UsersNameId[usrName] = CurrentAvaiId
+		UsersNameId[usrName] = CurrentNumOfId + 1
+		CurrentNumOfId++
 	}
-	usrID := UsersNameId[usrName]
-
-	usrSession := (usrID << 3 * 31) >> 2
-
-	// HERE IS WHERE WE PUT INITIAL GRAPH STATE INTO SESSION
-	TempData := Node{
-		Data:      "cow",
-		Id:        1,
-		Neighbors: []int{},
-	}
-
-	UsersIdSession[usrID] = usrSession
-	Sessions[usrSession] = append(Sessions[usrSession], TempData)
 
 	SessionKeys := []int{}
 	for k := range Sessions {
@@ -73,22 +67,47 @@ func GetUserSession(w http.ResponseWriter, r *http.Request) {
 }
 
 func UserExist(usrName string) bool {
-	if UsersNameId[usrName] == 0 {
-		return false
-	}
-	return true
+	_, ok := UsersNameId[usrName]
+	return ok
 }
 
 //var UsersNameId map[string]int // Mapping from UserName to Id
 //var UsersIdSession map[int]int // Mapping from Id to Session
 //var Sessions map[int][]Node    // Current running Sessions
-func connectToSession(w http.ResponseWriter, r *http.Request) {
+func ConnectToSession(w http.ResponseWriter, r *http.Request) {
 	usrName := r.FormValue("name")
-	usrID := UsersNameId[usrName]
 	usrSession, _ := strconv.Atoi(r.FormValue("session"))
+
+	/*
+		if newGame {
+			CurrentAvaiSession++
+			usrSession = CurrentAvaiSession
+		} else if _, ok := Sessions[usrSession]; !ok {
+			//Error. (new game not clicked, passed session does not exist
+		}
+	*/
+
+	if _, ok := Sessions[usrSession]; !ok {
+		fmt.Fprintf(w, "SESSION PASSED IN DOES NOT EXIST\n")
+	}
+	usrID := UsersNameId[usrName]
 	UsersIdSession[usrID] = usrSession
 
 	jsonFormattedData, _ := json.Marshal(Sessions[usrSession])
 	fmt.Printf("%s\n", jsonFormattedData)
 	fmt.Fprintf(w, "%s", jsonFormattedData)
+}
+
+func ParseData() []Node {
+	// HERE IS WHERE WE PUT INITIAL GRAPH STATE INTO SESSION
+	TempData := Node{
+		Data:      "cow",
+		Id:        1,
+		Neighbors: []int{},
+	}
+
+	GraphData := make([]Node, 50)
+
+	GraphData = append(GraphData, TempData)
+	return GraphData
 }
